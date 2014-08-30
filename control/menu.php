7e0ecd637b3211d422c9a63041b477224cbf42d4
Tyson Irby <tyson@builtbydevign.com>
2014-08-30 16:07:50 -0500

    ugh

--- control/menu.php
+++ control/menu.php
@@ -192,19 +192,6 @@
                   </div><!-- END Menu Item -->
                 </div>
 
-              <!--
-              <ul>
-                <li><a href=""><span class="glyphicon glyphicon-home"></span> Dashboard</a></li>
-                <li><a href=""><span class="glyphicon glyphicon-star"></span> Orders <span class="order-total">999</span></a></li>
-                <li><a href=""><span class="glyphicon glyphicon-shopping-cart"></span> Products <span class="general-total">999</span></a></li>
-                <li><a href=""><span class="glyphicon glyphicon-user"></span> Customers</a></li>
-                <li><a href=""><span class="glyphicon glyphicon-stats"></span> Reports</a></li>
-                <li><a href=""><span class="glyphicon glyphicon-file"></span> Pages</a></li>
-                <li><a href=""><span class="glyphicon glyphicon-picture"></span> Media</a></li>
-                <li><a href=""><span class="glyphicon glyphicon-pushpin"></span> My Notes <span class="general-total">3</span></a></li>
-              </ul>
-              -->
-
             </nav>
 
           </div><!-- /.col-sm-12 /.col-lg-9 -->
