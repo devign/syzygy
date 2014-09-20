6e36f1a671e37e3199942e84bb467f3ea6d28099
Tyson Irby <tyson@builtbydevign.com>
2014-09-20 18:02:28 -0500

    Login styling

--- control/login_form.php
+++ control/login_form.php
@@ -6,12 +6,16 @@ require_once('init.php');
 <html>
 <head>
 <title>SYZYGY LOGIN</title>
+
+<link href="/control/themes/<?= $config['theme'] . DSEP . $config['stylesheet_directory'] ?>styles.css" rel="stylesheet">
+<link href="/control/themes/<?= $config['theme'] . DSEP . $config['stylesheet_directory'] ?>custom.css" rel="stylesheet">
+
 </head>
 <body>
-<div id="pageContainer">
-    <div id="loginContainer">
+<div class="container" id="pageContainer">
+    <div class="col-sm-3 col-md-4 col-md-offset-4 mt90" id="loginContainer">
+        <div id="logo"><img src="themes/default/images/storefront-logo-default.png" border="0" width="67" height="30" /></div>
         <div id="loginBox">
-            <div id="logo"><img src="themes/default/images/storefront-logo-default.png" border="0" width="67" height="30" /></div>
             <?php if (count($errors) > 0) :?>
                 <div class="errorMessage">
                      <?php foreach ($errors as $error) : ?>
@@ -24,15 +28,11 @@ require_once('init.php');
              <div id="loginArea">
                 <form name="formLogin" method="post" action="login.php">
                 <input type="hidden" name="store_id" value="0">
-                <div class="inputLabel">USERNAME:</div>
-                <input autocomplete="off" type="text" name="username" value="" size="25">
-                
-                <br/>
-                <br/>
-                <div class="inputLabel">PASSWORD:</div>
-                <input autocomplete="off" type="password" name="password" value="" size="25">
-                <br/><br/>
-                <input type="submit" value="Log In">
+                <div class="inputLabel">Username:</div>
+                <input class="login-form-input" autocomplete="off" type="text" name="username" value="">
+                <div class="inputLabel">Password:</div>
+                <input class="login-form-input" autocomplete="off" type="password" name="password" value="">
+                <input class="login-button" type="submit" value="Log In">
                 </form>
             </div>
         </div>
